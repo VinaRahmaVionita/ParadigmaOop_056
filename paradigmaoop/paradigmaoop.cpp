48a1ce60057aeb8a05eee5678c48dda0a986f51d
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-24 17:16:45 +0700

    membuat kelas nama dan set ekses ke publik, memiliki data member, konstruktor, dan destruktor serta member funtion

--- paradigmaoop/paradigmaoop.cpp
+++ paradigmaoop/paradigmaoop.cpp
@@ -1,20 +1,46 @@
-// paradigmaoop.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream> 
+#include <string>
+using namespace std;
 
-#include <iostream>
+class orang {
+public:
+	string nama;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+
+};
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya" + nama + " dari sekolah " + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar siswa1(" andi laksono", "SMAN 1 BANTUL");
+	cout << siswa1.perkenalan();
+	cout << "Hasil =" << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

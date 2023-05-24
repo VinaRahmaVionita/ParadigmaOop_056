f98ae4e7f7fb0e7d5f2453587bb9d3d4e870f7d1
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-24 17:36:58 +0700

    membuat kelas dokter, set akses ke publik string nama unyuk mendeklarasikan nama, vector<pasien> berisi daftar nama nama pasien, terdapat konstruktor dan destruktor disana, fungsi void cetak pasien di gunakan mencetak daftar pasien sedangkan fungsi void tambah pasien di gunakan untuk menambah pasien yg baru saja mendaftar

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -5,15 +5,30 @@ using namespace std;
 class dokter;
 class pasien {
 public:
-	string nama;
+	string nama;                            
 	vector<dokter*> daftar_dokter;
-	pasien(string pNama) :nama(pNama) {
+	pasien(string pNama) :nama(pNama) {             //konstruktor 
 		cout << "Pasien \"" << nama << "\" ada\n";
 	}
-	~pasien() {
+	~pasien() {                                     //destruktor
 		cout << "Pasien \"" << nama << "\" tidak ada\n";
 	}
 	void tambahDokter(dokter*);
 	void cetakDokter();
+};
+class dokter {
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {               //konstruktor
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {                                           //destruktor
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
+
+	void tambahPasien(pasien*);
+	void cetakPasien();
 };
 
